72f130c78d1798cbd5319060a0892922287d6739
Ian Becker <ian@beckr.dev>
2024-06-26 15:52:51 -0400

    fix: corrects behavior when using bundled `treefmt` and allows config setting for custom binary path

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,14 @@
 # Change Log
 
-All notable changes to the "treefmt-vscode" extension will be documented in this file.
-
-Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
-
-## [Unreleased]
+## 0.0.1
 
 - Initial release
+
+## 0.0.2
+
+- Fixed bundled binary usage
+- Added configuration options
+  - Specify custom treefmt binary path
+  - Specify custom `treefmt.toml` configuration file path
+- Added new init command
+- Added icon
--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,40 @@
-import * as vscode from "vscode";
-import * as fs from "fs";
-import * as path from "path";
 import { exec } from "child_process";
+import * as fs from "fs";
+import { homedir } from "os";
+import * as path from "path";
+import * as vscode from "vscode";
+
+let ctx: vscode.ExtensionContext;
+let command: string;
+let configPath: string;
+
+async function getWorkspaceRoot() {
+	const workspaceFolders = vscode.workspace.workspaceFolders;
+	if (!workspaceFolders) {
+		vscode.window.showInformationMessage("No workspace folder found.");
+		return;
+	}
+
+	return workspaceFolders[0].uri.fsPath;
+}
+
+async function initTreefmt() {
+	const workspaceRoot = await getWorkspaceRoot();
+	if (!workspaceRoot) {
+		vscode.window.showInformationMessage("No workspace root found.");
+		return;
+	}
+
+	await readConfig();
+
+	exec(`${command} --init`, { cwd: workspaceRoot }, (error) => {
+		if (error) {
+			vscode.window.showErrorMessage(`Error running ${command} --init`);
+		} else {
+			vscode.window.showInformationMessage("Created treefmt.toml");
+		}
+	});
+}
 
 async function runTreefmt() {
 	const editor = vscode.window.activeTextEditor;
@@ -9,26 +42,15 @@ async function runTreefmt() {
 		vscode.window.showInformationMessage("No active editor found.");
 		return;
 	}
-
-	const document = editor.document;
-	const filePath = document.fileName;
-	const workspaceFolders = vscode.workspace.workspaceFolders;
-
-	if (!workspaceFolders) {
-		vscode.window.showInformationMessage("No workspace folder found.");
+	const workspaceRoot = await getWorkspaceRoot();
+	if (!workspaceRoot) {
+		vscode.window.showInformationMessage("No workspace root found.");
 		return;
 	}
 
-	const workspaceRoot = workspaceFolders[0].uri.fsPath;
-	const treefmtTomlPath = path.join(workspaceRoot, "treefmt.toml");
-	const bundledTreefmtPath = path.join(__dirname, "bin", "treefmt");
+	await readConfig();
 
-	let treefmtCommand = "treefmt";
-	if (fs.existsSync(bundledTreefmtPath)) {
-		treefmtCommand = bundledTreefmtPath;
-	}
-
-	if (!fs.existsSync(treefmtTomlPath)) {
+	if (!fs.existsSync(configPath)) {
 		const create = await vscode.window.showInformationMessage(
 			"No treefmt.toml found. Would you like to create one?",
 			"Yes",
@@ -36,73 +58,68 @@ async function runTreefmt() {
 		);
 
 		if (create === "Yes") {
-			exec(`${treefmtCommand} --init`, { cwd: workspaceRoot }, (error) => {
-				if (error) {
-					vscode.window.showErrorMessage(
-						`Error running ${treefmtCommand} --init`,
-					);
-				} else {
-					vscode.window.showInformationMessage("Created treefmt.toml");
-				}
-			});
-		} else {
-			return;
+			initTreefmt();
 		}
+
+		// Early return to prevent running treefmt with a config file that isn't configured
+		// treefmt --init makes a treefmt.toml file but it doesn't have any rules in it
+		return;
 	}
 
-	exec(treefmtCommand, { cwd: workspaceRoot }, (error, stdout, stderr) => {
-		if (error) {
-			vscode.window.showErrorMessage(
-				`Error running ${treefmtCommand}: ${stderr}`,
-			);
-			return;
-		}
-		vscode.window.showInformationMessage("treefmt ran successfully.");
-	});
+	exec(
+		`${command} --config-file=${configPath}`,
+		{ cwd: workspaceRoot },
+		(error, stdout, stderr) => {
+			if (error) {
+				vscode.window.showErrorMessage(`Error running ${command}: ${stderr}`);
+				return;
+			}
+			vscode.window.showInformationMessage("treefmt ran successfully.");
+		},
+	);
+}
+
+async function readConfig() {
+	const workspaceRoot = await getWorkspaceRoot();
+	const config = vscode.workspace.getConfiguration("treefmt");
+	const treefmtCommand: string | null = config.get("command") as string | null;
+	if (treefmtCommand) {
+		command = treefmtCommand;
+	} else {
+		const ext = process.platform === "win32" ? ".exe" : "";
+		command = vscode.Uri.joinPath(
+			ctx.extensionUri,
+			"bin",
+			`treefmt${ext}`,
+		).fsPath;
+	}
+	if (command.startsWith("~/")) {
+		command = homedir() + command.slice("~".length);
+	}
+
+	const treefmtTomlPath: string | null = config.get("config") as string | null;
+	if (treefmtTomlPath) {
+		configPath = treefmtTomlPath;
+	} else {
+		configPath = path.join(workspaceRoot ?? "", "treefmt.toml");
+	}
 }
 
 export function activate(context: vscode.ExtensionContext) {
+	ctx = context;
+
 	context.subscriptions.push(
 		vscode.commands.registerCommand("extension.runTreefmt", runTreefmt),
+		vscode.commands.registerCommand("extension.initTreefmt", initTreefmt),
 	);
 
 	vscode.languages.registerDocumentFormattingEditProvider(
 		{ pattern: "**/*" },
 		{
-			provideDocumentFormattingEdits(
+			async provideDocumentFormattingEdits(
 				document: vscode.TextDocument,
-			): vscode.TextEdit[] {
-				const workspaceFolders = vscode.workspace.workspaceFolders;
-				if (!workspaceFolders) {
-					return [];
-				}
-
-				const workspaceRoot = workspaceFolders[0].uri.fsPath;
-				const treefmtTomlPath = path.join(workspaceRoot, "treefmt.toml");
-				const bundledTreefmtPath = path.join(__dirname, "bin", "treefmt");
-
-				let treefmtCommand = "treefmt";
-				if (fs.existsSync(bundledTreefmtPath)) {
-					treefmtCommand = bundledTreefmtPath;
-				}
-
-				if (!fs.existsSync(treefmtTomlPath)) {
-					return [];
-				}
-
-				exec(
-					treefmtCommand,
-					{ cwd: workspaceRoot },
-					(error, stdout, stderr) => {
-						if (error) {
-							vscode.window.showErrorMessage(
-								`Error running ${treefmtCommand}: ${stderr}`,
-							);
-							return [];
-						}
-						vscode.window.showInformationMessage("treefmt ran successfully.");
-					},
-				);
+			): Promise<vscode.TextEdit[]> {
+				await runTreefmt();
 
 				return [];
 			},
